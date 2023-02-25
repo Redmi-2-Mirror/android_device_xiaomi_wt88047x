cca7afda132191c7db3b2cfe1803f5162731a38a
rik0612c <qq872638399@gmail.com>
2023-02-25 12:47:09 +0800

    wt88047x: drop Snap

--- device.mk
+++ device.mk
@@ -52,8 +52,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8916 \
-    libshim_camera \
-    Snap
+    libshim_camera
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
